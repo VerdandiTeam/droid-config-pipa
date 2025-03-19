3f7a55862d34362c20fff3534cff7cb3b39cbbc4
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2025-03-19 01:12:12 +0100

    Fix wifi once and for all

--- sparse/usr/bin/droid/wait-for-wifi.sh
+++ sparse/usr/bin/droid/wait-for-wifi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ip a | grep wlan0 > /dev/null
+ip a | grep "wlan\|wlp" > /dev/null
 
-while [ $? -eq 1 ]; do sleep 1; ip a | grep wlan0 > /dev/null; done
+while [ $? -eq 1 ]; do sleep 1; ip a | grep "wlan\|wlp" > /dev/null; done
 
 exit 0
